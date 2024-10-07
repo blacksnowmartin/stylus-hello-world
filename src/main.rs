6aa0d0a73bebfa5373c4e3e949739289974c0317
blacksnowmartin <martinmwendwa005@gmail.com>
2024-10-08 00:03:41 +0300

    first complete code from youtube tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use eyre::Ok;
+#![cfg_attr(not(feature = "export-abi", no_main))]
+
+#[global_allocator]
+static ALLOC: wee_alloc:  ::wee_alloc::WeeAlloc> = wee_alloc: ::wee_alloc::Wee
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
-sol storage! {
+sol_storage! {
     #[entrypoint]
     pub struct Counter {
         uint256 number;
@@ -9,5 +14,16 @@ sol storage! {
 
 #[external]
 impl Counter {
-   pub fn number(&self) -> Result<U256, Vec<un> {
+   pub fn number(&self) -> Result<U256, Vec<u8>> {
+       Ok(self.number.get())
+   }
+   pub fn set_number(&mut self, number: U256) -> Result<(), Vec<u8>> {
+       self.number.set(new_number);
+       Ok(())
+   }
+   pub fn increment(&mut self)  -> Result<(), Vec<u8>> {
+    let number: Uint<256, 4> = self.number.get();
+    self.number.set(number + U256::from(1));
+    Ok(())
+   }
 }
