aa3170d4a5ec26a522148eab0986295888fc11e7
blacksnowmartin <martinmwendwa005@gmail.com>
2024-10-12 12:17:55 +0300

    renamed .env

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
-#[cfg(feature = "export-abi")]
-fn main() {
-    stylus_hello_world::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+sol_storage! {
+    #[entrypoint]
+    pub struct Counter {
+        uint256 number;
+        }
 }
+
+#[external]
+impl Counter {
+   pub fn number(&self) -> Result<U256, Vec<u8>> {
+       Ok(self.number.get())
+   }
+   pub fn set_number(&mut self, new_number: U256) -> Result<(), Vec<u8>> {
+       self.number.set(new_number);
+       Ok(())
+   }
+   pub fn increment(&mut self)  -> Result<(), Vec<u8>> {
+    let number: Uint<256, 4> = self.number.get();
+    self.number.set(number + U256::from(1));
+    Ok(())
+   }
+}
+
